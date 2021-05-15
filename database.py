3b8c79dd458e2d4e3398403daed2624fa6592c22
Dicle Acet <71029563+fawmain@users.noreply.github.com>
2021-05-15 15:32:30 +0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,6 @@ def createTable():
     connection.execute("CREATE TABLE USERS (USERNAME TEXT NOT NULL, EMAIL TEXT, PASSWORD TEXT, NAME TEXT, TC İNT, PHONE İNT, ADRESS TEXT, ADMIN İNT, HESAPBAKİYE İNT)")
     connection.execute("CREATE TABLE PRODUCT(USERNAME TEXT,PRODUCTNAME TEXT,PRİCE İNT, PRODUCTQUANTİTY İNT)")
     connection.execute("INSERT INTO USERS VALUES(?,?,?,?,?,?,?,?,?)",('dicle','dicle@gmail.com','123','dicleacet','12112112112','05055050550','izmir','1','30000'))
-    connection.commit()
     connection.execute("CREATE TABLE BAKIYEPENDİNG (USERNAME TEXT NOT NULL, BAKİYE İNT)")
     connection.execute("CREATE TABLE PRODUCTPENDİNG(USERNAME TEXT,PRODUCTNAME TEXT,PRİCE İNT, PRODUCTQUANTİTY İNT)")
     connection.commit()
