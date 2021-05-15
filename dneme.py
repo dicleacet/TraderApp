91679a59cc0790714e5ee12cdf4f8e4f90100478
Dicle Acet <71029563+fawmain@users.noreply.github.com>
2021-05-15 15:17:00 +0300

    Delete dneme.py

--- dneme.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("TraderDb.db")
-cursor = connection.cursor()       
-product = cursor.execute("INSERT INTO PRODUCT VALUES(?,?,?,?)",("rafik","Arpa",1,33))
-connection.commit()
-connection.close()
